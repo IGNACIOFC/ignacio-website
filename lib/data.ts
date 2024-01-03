9817da4a30fe3dc65cb2d9d4eea52eb3c6923479
Ignacio Fernandez <ifernandezcerro@gmail.com>
2024-01-03 13:43:38 +0100

    projects

--- lib/data.ts
+++ lib/data.ts
@@ -5,6 +5,12 @@ import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
+import didacticImg from "@/public/didactic.png";
+import buscolibroImg from "@/public/buscolibro.png";
+import estapasandoImg from "@/public/estapasando.png";
+import appImg from "@/public/app.png";
+import iceDriveImg from "@/public/icedrive.png";
+import readlyImg from "@/public/readly.png";
 
 export const links = [
   {
@@ -58,25 +64,25 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
-  },
-  {
     title: "rmtDev",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    imageUrl: iceDriveImg,
+  },
+  {
+    title: "CorpComment",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: appImg,
   },
   {
     title: "Word Analytics",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: estapasandoImg,
   },
 ] as const;
 

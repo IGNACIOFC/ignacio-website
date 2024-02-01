f93703aba16ba8347d52674a798d8e0207c06c99
Ignacio Fernandez <ifernandezcerro@gmail.com>
2024-02-01 19:09:06 +0100

    change intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -61,11 +61,8 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I&apos;m Ignacio.</span> I&apos;m a{" "}
-        <span className="font-bold">software developer</span> with{" "}
-        <span className="font-bold">4 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        
+        <span className="font-bold">Hello, I'm Ignacio from Madrid,</span> and I'm currently planning to relocate to Dublin soon. I'm a <span className="font-bold">software developer</span> with <span className="font-bold">4 years</span> of experience, specializing in building <span className="italic">websites and applications</span> with a focus on <span className="underline">React, particularly Next.js</span>.
       </motion.h1>
 
       <motion.div
